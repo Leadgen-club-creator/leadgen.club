3f872c94bf8e0c50384486f42424d00709b018fe
Glader <glader.ru@gmail.com>
2022-10-23 20:31:48 +0400

    Email fix

--- posts/context_processors/topics.py
+++ posts/context_processors/topics.py
@@ -2,7 +2,7 @@ from posts.models.topics import Topic
 
 
 def topics(request):
-    topics = Topic.objects.filter(is_visible=True).all().order_by("id")
+    topics = Topic.objects.filter(is_visible=True).all()
     return {
         "topics": Topic.objects.filter(is_visible=True).all(),
         "topics_map": {t.slug: t for t in topics},
