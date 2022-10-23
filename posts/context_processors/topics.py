af9bd5bd325a76a6ca43b63764eba4ddd085dfdd
Glader <glader.ru@gmail.com>
2022-10-23 15:55:44 +0400

    Order topics

--- posts/context_processors/topics.py
+++ posts/context_processors/topics.py
@@ -2,7 +2,7 @@ from posts.models.topics import Topic
 
 
 def topics(request):
-    topics = Topic.objects.filter(is_visible=True).all()
+    topics = Topic.objects.filter(is_visible=True).all().order_by("id")
     return {
         "topics": Topic.objects.filter(is_visible=True).all(),
         "topics_map": {t.slug: t for t in topics},
