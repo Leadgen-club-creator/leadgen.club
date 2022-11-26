667d99b314f887b617fabf8d83af99d3f6d22c66
Mikhail <glader.ru@gmail.com>
2022-11-26 22:58:23 +0400

    Fix club name

--- club/settings.py
+++ club/settings.py
@@ -171,7 +171,7 @@ DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "glader.ru@gmail.com")
 # App
 
 APP_HOST = os.environ.get("APP_HOST") or "http://127.0.0.1:8000"
-APP_NAME = "Вастрик.Клуб"
+APP_NAME = "Лидген.Клуб"
 APP_DESCRIPTION = "Всё интересное происходит за закрытыми дверями"
 LAUNCH_DATE = datetime(2020, 4, 13)
 
